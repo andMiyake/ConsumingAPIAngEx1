d2bca18cba8ec2ad317e8659d02b6019a750a3f6
Anderson Miyake <afmyk@hotmail.com>
2024-02-13 16:35:13 -0300

    API consuming exercise in Angular.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ConsumingAPIAngEx1';
+  advices: any;
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    this.http.get('https://api.adviceslip.com/advice').subscribe({
+      next: response => this.advices = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed.')
+    })
+  }
+
+
+
 }
